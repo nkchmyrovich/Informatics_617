eb72d1cb744f101e2a0e9c4b173b7b0d594c3354
Vlad <vlad.miryaha@gmail.com>
2017-12-08 07:38:58 +0300

    fixit

--- file_find.c
+++ file_find.c
@@ -7,8 +7,27 @@
 
 #define MAX_STR_LEN 255
 
+/*
+FIXIT:
+параметр _depth выглядит мутно.
+представьте, что вы хотите воспользоваться этой ф-ей. что подставить в качестве последнего параметра? 
+вот и я не знаю. вы подставляете 0.
+
+обычно в таких случаях делают так: создают FindFileImpl с 4мя параметрами.
+
+а ещё FindFile c тремя понятными параметрами, которая внутри себя вызывает FindFileImpl и подставляет
+магический 0 ... "пользователя" не нужно посвещать в детали вашей реализации ...
+*/
 int FindFile (char* dir_name, size_t depth, char* file_name, size_t _depth) {
+	/*
+	зачем слово static тут?
+	*/
 	static bool ignore = false;
+	
+	/*
+	почему нельзя из depth было вычитать 1?
+	и остановиться, когда она станет равной 0 ... без ввода дополнительной переменной
+	*/
 	_depth++;
 	if (_depth > depth) { 
 		return 1; 
@@ -20,11 +39,18 @@ int FindFile (char* dir_name, size_t depth, char* file_name, size_t _depth) {
         }
         struct dirent* dir_rec;
 	while (dir_rec = readdir(dir_stream)) {
+		/*
+		fixit: пробелы вокруг бинарных операторов
+		*/
 		if (!strcmp(dir_rec->d_name, ".")||!strcmp(dir_rec->d_name, "..")) {
 			ignore = true;
 		} else {
 			ignore = false;
 		}
+		/*
+		можно было бы просто ignore = !strcmp(dir_rec->d_name, ".") || !strcmp(dir_rec->d_name, "..");
+		без 5 строк кода
+		*/
 		if (!ignore) {
 			if (!strcmp(dir_rec->d_name, file_name)) {
 				printf("File was found.\nDirectory: %s\n", dir_name);
@@ -42,12 +68,16 @@ int FindFile (char* dir_name, size_t depth, char* file_name, size_t _depth) {
 	}
 }
 	
-	
-
 int main(int argc, char** argv) {
 	char* file_name = (char*)calloc(MAX_STR_LEN, sizeof(char));
 	char* dir_name = (char*)calloc(MAX_STR_LEN, sizeof(char));
+	/*
+	По-хорошему сюда стоит добавить проверку количества входных паратеров.
+	*/
 	size_t depth = atoi(argv[2]);
+	/*
+	А также проверить, получилось ли сконвертировать строку в число.
+	*/
 	strcpy(file_name, argv[3]);
 	strcpy(dir_name, argv[1]);
 	if (FindFile(dir_name, depth, file_name, 0)) {
