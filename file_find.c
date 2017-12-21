1d24c2b65f0228920cdc7d978ec50ebde096e8cf
nkchmyrovich <nkchmyrovich1@gmail.com>
2017-12-22 01:16:00 +0300

    ...

--- file_find.c
+++ file_find.c
@@ -4,11 +4,19 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #define MAX_STR_LEN 255
+#define PARAMAMOUNT 4
+
+enum CMD_INPUT {
+	DIR_NAME = 1,
+	DEPTH = 2, //2
+	FILE_NAME = 3 //3
+};
 
 int FindFile (char* dir_name, size_t depth, char* file_name, size_t _depth) {
-	static bool ignore = false;
+	bool ignore = false;
 	_depth++;
 	if (_depth > depth) { 
 		return 1; 
@@ -20,11 +28,7 @@ int FindFile (char* dir_name, size_t depth, char* file_name, size_t _depth) {
         }
         struct dirent* dir_rec;
 	while (dir_rec = readdir(dir_stream)) {
-		if (!strcmp(dir_rec->d_name, ".")||!strcmp(dir_rec->d_name, "..")) {
-			ignore = true;
-		} else {
-			ignore = false;
-		}
+		ignore = !strcmp(dir_rec->d_name, ".") || !strcmp(dir_rec->d_name, "..");
 		if (!ignore) {
 			if (!strcmp(dir_rec->d_name, file_name)) {
 				printf("File was found.\nDirectory: %s\n", dir_name);
@@ -45,14 +49,28 @@ int FindFile (char* dir_name, size_t depth, char* file_name, size_t _depth) {
 	
 
 int main(int argc, char** argv) {
+	if (argc != PARAMAMOUNT) {
+		printf("Incorrect amount of cmd params.\n");
+		return 1;
+	}
+	if (!argv[DIR_NAME] || !argv[DEPTH] || !argv[FILE_NAME]) {
+		printf("Incorrect input.\n");
+		return 1;
+	}
+	if (!isdigit(argv[DEPTH])) {
+		printf("Incorrect depth.\n");
+		return 1;
+	}
 	char* file_name = (char*)calloc(MAX_STR_LEN, sizeof(char));
-	char* dir_name = (char*)calloc(MAX_STR_LEN, sizeof(char));
-	size_t depth = atoi(argv[2]);
-	strcpy(file_name, argv[3]);
-	strcpy(dir_name, argv[1]);
+        char* dir_name = (char*)calloc(MAX_STR_LEN, sizeof(char));
+	size_t depth = atoi(argv[DEPTH]);
+	strcpy(file_name, argv[FILE_NAME]);
+	strcpy(dir_name, argv[DIR_NAME]);
 	if (FindFile(dir_name, depth, file_name, 0)) {
 		printf("File not found\n");
 	}
+	free(file_name);
+	free(dir_name);
 	return 0;
 }	
 	
